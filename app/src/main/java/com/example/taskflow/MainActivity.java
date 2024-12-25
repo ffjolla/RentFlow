f5abcd58a781bf5ca2000cc76f471b377d6245f1
fjollakadriu <154750535+ffjolla@users.noreply.github.com>
2024-12-25 21:12:50 +0100

    main activity show

--- app/src/main/java/com/example/taskflow/MainActivity.java
+++ app/src/main/java/com/example/taskflow/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskflow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        },4000);
     }
 }
